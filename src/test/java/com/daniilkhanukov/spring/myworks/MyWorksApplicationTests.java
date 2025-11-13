a332cb2920ba3544c6e21849b98755bead21eae5
Daniil Khanukov <d.hanukoff@yandex.ru>
2025-11-13 16:46:56 +0300

    init commit

---
+++ src/test/java/com/daniilkhanukov/spring/myworks/MyWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniilkhanukov.spring.myworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
