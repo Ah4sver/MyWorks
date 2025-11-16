317ad4f7a4a729dc68c78a669ee06ae80dd2e457
Daniil Khanukov <d.hanukoff@yandex.ru>
2025-11-16 15:14:35 +0300

    Выполнение задачи 3

---
+++ src/main/java/com/daniilkhanukov/spring/myworks/RingBuffer.java
@@ -0,0 +1,88 @@
+package com.daniilkhanukov.spring.myworks;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RingBuffer {
+    private final int[] buffer;
+    private final int capacity;
+    private int head = 0;
+    private int tail = 0;
+    private int size = 0;
+
+    public RingBuffer(int capacity) {
+        this.capacity = capacity;
+        buffer = new int[capacity];
+    }
+
+    private final Lock lock = new ReentrantLock();
+    private final Condition notEmpty = lock.newCondition();
+    private final Condition notFull = lock.newCondition();
+
+    public void put(int value) throws InterruptedException {
+        lock.lock();
+        try {
+            while (size == capacity) {
+                notFull.await();
+            }
+            buffer[tail] = value;
+            tail = (tail + 1) % capacity;
+            size++;
+            notEmpty.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int get() throws InterruptedException {
+        lock.lock();
+        try {
+            while (size == 0) {
+                notEmpty.await();
+            }
+            int value = buffer[head];
+            head = (head + 1) % capacity;
+            size--;
+            notFull.signal();
+            return value;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        RingBuffer ringBuffer = new RingBuffer(5);
+
+        Thread producer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 23; i++) {
+                    ringBuffer.put(i);
+                    System.out.println("Вставлено значение " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 23; i++) {
+                    int item = ringBuffer.get();
+                    System.out.println("Получено значение: " + item);
+                    Thread.sleep(2500);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+        System.out.println("Конец");
+    }
+}
